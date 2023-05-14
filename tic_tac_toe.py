ad2e79649e64f72ac86ab9fa23d95cb830fa441c
Adesh Pramod Yadav <95179737+adeshyadav2020@users.noreply.github.com>
2023-05-14 11:26:08 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,4 +3,12 @@ import time
     
 board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']    
 player = 1    
+########win Flags##########    
+Win = 1    
+Draw = -1    
+Running = 0    
+Stop = 1    
+###########################    
+Game = Running    
+Mark = 'X'    
    
