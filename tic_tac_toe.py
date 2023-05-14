2077d29b5e1e8ac772a93ab6eb98c2e3fc84c975
Adesh Pramod Yadav <95179737+adeshyadav2020@users.noreply.github.com>
2023-05-14 11:26:35 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -11,4 +11,12 @@ Stop = 1
 ###########################    
 Game = Running    
 Mark = 'X'    
+#This Function Draws Game Board    
+def DrawBoard():    
+    print(" %c | %c | %c " % (board[1],board[2],board[3]))    
+    print("___|___|___")    
+    print(" %c | %c | %c " % (board[4],board[5],board[6]))    
+    print("___|___|___")    
+    print(" %c | %c | %c " % (board[7],board[8],board[9]))    
+    print("   |   |   ")    
    
